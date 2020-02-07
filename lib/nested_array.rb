21b0135cae73876ae15ad88e21aefa296dd14c57
Nduka-Eze Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-07 21:59:48 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -33,9 +33,7 @@ def sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-  matrix = []
-  matrix << column = row
-  matrix
+  matrix[column][row]
 end
 
 def matrix_update(matrix, row, column, new_value)
